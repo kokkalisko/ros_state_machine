b31935ec72ef5521eb543f3566262f31229d38b1
Konstantinos Kokkalis <kokkalisko@gmail.com>
2021-11-20 11:59:33 +0200

    Update CHANGELOG

---
+++ CHANGELOG.rst
@@ -0,0 +1,9 @@
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Changelog for package ros_state_machine
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+Forthcoming
+-----------
+* First iteration of state machine
+* Initial commit
+* Contributors: Konstantinos Kokkalis
--- src/state_machine_node.cpp
+++ src/state_machine_node.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "state_machine_node");
     MoveStateMachine stateMachine;
     stateMachine.DefineTransitions();
-    
+
     const Trigger TriggerVector[] = { Trigger::MOVE, Trigger::STOP, Trigger::RETREAT };
     std::string operation;
 
@@ -16,9 +16,8 @@ int main(int argc, char **argv)
     {
         ROS_INFO("Current robot state: %s", ResolveState(stateMachine.GetCurrentState()).c_str());
         for ( auto trigger: TriggerVector )
-        {
             std::cout << static_cast<int>(trigger) << ": " << ResolveTrigger(trigger) << std::endl;
-        }
+
         std::cin >> operation;
         ProcessTransitionResult result = stateMachine.ProcessTrigger(static_cast<Trigger>(stoi(operation)));
         ROS_INFO("%s", ResolveTransitionResult(result).c_str());
